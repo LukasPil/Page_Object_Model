641d7c462eb3cdf23a8433a157117ca68f730188
LukasPil <piliponis.lukas@gmail.com>
2022-07-11 13:53:26 +0300

    Add test for single input field

--- src/test/java/pages/seleniumEasy/SimpleFormDemo.java
+++ src/test/java/pages/seleniumEasy/SimpleFormDemo.java
@@ -5,18 +5,33 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.Common;
+import pages.Locators;
 import utils.Driver;
 
 import java.time.Duration;
 
 public class SimpleFormDemo {
+
     public static void open() {
         pages.Common.openUrl("https://demo.seleniumeasy.com/basic-first-form-demo.html");
     }
 
     public static void closeAd() {
-        Common.waitForElementToBeVisiable(By.xpath("//div[@id='at-cv-lightbox-content']"));
-        Common.clickElement(By.xpath("//*[@id='at-cv-lightbox-close']"));
+        Common.waitForElementToBeVisiable(Locators.SeleniumEasy.SimpleFormDemo.formAd);
+        Common.clickElement(Locators.SeleniumEasy.SimpleFormDemo.buttonCloseAd);
 
     }
+
+    public static void enterMessage(String expectedMessage) {
+        Common.sendKeysToElement(By.xpath("//input[@id='user-message']"), expectedMessage);
+    }
+
+    public static void clickShowMessageButton() {
+        Common.clickElement(Locators.SeleniumEasy.SimpleFormDemo.buttonShowMessage);
+    }
+
+    public static String readMessage() {
+        return Common.getElementText(Locators.SeleniumEasy.SimpleFormDemo.fieldMessage);
+    }
+
 }
