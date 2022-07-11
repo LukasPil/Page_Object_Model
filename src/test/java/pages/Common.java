cd50b3597c37dfc3cfeedcd005ca068731924ee7
LukasPil <piliponis.lukas@gmail.com>
2022-07-11 13:52:45 +0300

    Add elements into Common class

--- src/test/java/pages/Common.java
+++ src/test/java/pages/Common.java
@@ -14,14 +14,25 @@ public class Common {
         Driver.getDriver().get(url);
     }
 
-    public static void clickElement(By locator) {
-
-        WebElement buttonCloseAd = Driver.getDriver().findElement(locator);
-        buttonCloseAd.click();
-    }
-
     public static void waitForElementToBeVisiable(By locator) {
         WebDriverWait webDriverWait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+
+    public static WebElement getElement(By locator) {
+        return Driver.getDriver().findElement(locator);
+    }
+
+    public static void clickElement(By locator) {
+        getElement(locator).click();
+    }
+
+    public static void sendKeysToElement(By locator, String keys) {
+        getElement(locator).sendKeys(keys);
+    }
+
+    public static String getElementText(By locator) {
+        return getElement(locator).getText();
     }
 }
